3bebaac940a4df24d0bad2adf2c1468b3763228f
varad8 <varadnikharage201@gmail.com>
2025-01-05 18:48:19 +0530

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,46 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://vrnitsolution.netlify.app",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://vrnitsolution.netlify.app/project",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vrnitsolution.netlify.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vrnitsolution.netlify.app/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vrnitsolution.netlify.app/service",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vrnitsolution.netlify.app/project/todo-webapp-using-nextjs-and-mongodb",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vrnitsolution.netlify.app/project/ev-glob-online-ev-station-booking-webapp",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
